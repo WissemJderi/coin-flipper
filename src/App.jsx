06facb21880d777d17068c485e2c8f0839d5079d
Wissem Jderi <asqr43641@gmail.com>
2025-03-14 10:31:34 +0100

    implement coin flipper functionality and remove unused CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentCoin, setCurrentCoin] = useState();
+  const [numberOfHeads, setNumberOfHeads] = useState(0);
+  const [numberOfTails, setNumberOfTails] = useState(0);
 
+  const coinFaces = ["Head", "Tail"];
+  function flipCoin() {
+    let randomNum = Math.floor(Math.random() * coinFaces.length);
+    const newCoin = coinFaces[randomNum];
+    setCurrentCoin(newCoin);
+    if (newCoin === coinFaces[0]) {
+      setNumberOfHeads(numberOfHeads + 1);
+    } else if (newCoin === coinFaces[1]) {
+      setNumberOfTails(numberOfTails + 1);
+    }
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Coin Flipper</h1>
+      <h2>Flip The Coin</h2>
+      <h2 style={{ color: "purple" }}>{currentCoin}</h2>
+      <button
+        onClick={() => {
+          flipCoin();
+        }}
+      >
+        Flip me
+      </button>
+      <h3>
+        Out of {numberOfHeads + numberOfTails} flips, there have been{" "}
+        {numberOfHeads} heads and {numberOfTails} tails
+      </h3>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
